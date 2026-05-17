b70e9b21fbab7396aa06ecdd7aa8e6ac12d508ef
Parveen440 <parveenshaik0231@gmail.com>
2026-05-17 23:37:22 +0530

    Add Climbing Stairs DP solution

---
+++ src/dp/ClimbingStairs.java
@@ -0,0 +1,18 @@
+package dp;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        // Handle base cases
+        if (n <= 2) {
+            return n;
+        }
+        int dp[] = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        // Fill DP array
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
