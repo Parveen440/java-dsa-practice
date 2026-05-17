f3051bb38e75ee6f30497d1ebb8cfcf4b7d72af1
Parveen440 <parveenshaik0231@gmail.com>
2026-05-17 23:38:53 +0530

    Update ClimbingStairs.java

--- src/dp/ClimbingStairs.java
+++ src/dp/ClimbingStairs.java
@@ -2,14 +2,14 @@ package dp;
 
 public class ClimbingStairs {
     public int climbStairs(int n) {
-        // Handle base cases
+        // handle base cases
         if (n <= 2) {
             return n;
         }
         int dp[] = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
-        // Fill DP array
+        // fill DP array
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
