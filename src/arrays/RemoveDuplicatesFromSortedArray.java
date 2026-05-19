b488002e1900f2b0590e6e9394e8281166d98632
Parveen440 <parveenshaik0231@gmail.com>
2026-05-19 23:53:28 +0530

    Update RemoveDuplicatesFromSortedArray.java

--- src/arrays/RemoveDuplicatesFromSortedArray.java
+++ src/arrays/RemoveDuplicatesFromSortedArray.java
@@ -2,28 +2,21 @@ package arrays;
 //Leetcode-26
 public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
-
         int n = nums.length;
-
         // Edge case
         if (n == 0) {
             return 0;
         }
-
         // Pointer for unique elements
         int k = 1;
-
         // Traverse array
         for (int i = 1; i < n; i++) {
-
             // If current element is unique
             if (nums[i] != nums[i - 1]) {
-
                 nums[k] = nums[i];
                 k++;
             }
         }
-
         return k;
     }
 
