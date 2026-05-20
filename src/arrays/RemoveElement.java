f8f03ba1d5e3c5169aac7c653d5ba4c91d3824d9
Parveen440 <parveenshaik0231@gmail.com>
2026-05-20 23:40:43 +0530

    Add Remove Element solution

--- src/arrays/RemoveElement.java
+++ src/arrays/RemoveElement.java
@@ -1,4 +1,18 @@
 package arrays;
-
+//Leetcode-27
 public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int start = 0;
+        // Traverse array
+        for (int i = 0; i < nums.length; i++) {
+            // Keep only non-val elements
+            if (nums[i] != val) {
+                nums[start] = nums[i];
+                start++;
+            }
+        }
+        return start;
+    }
 }
+//Time Complexity: O(n)
+//Space Complexity: O(1)
