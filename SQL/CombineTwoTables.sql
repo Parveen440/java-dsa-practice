33ee2d418831f6776325a4b8c856fae26b63bd53
Parveen440 <parveenshaik0231@gmail.com>
2026-06-05 22:40:19 +0530

    Add Combine Two Tables SQL solution

---
+++ SQL/CombineTwoTables.sql
@@ -0,0 +1,10 @@
+-- LeetCode 175: Combine Two Tables
+-- Topic: LEFT JOIN
+SELECT
+Person.firstName,
+Person.lastName,
+Address.city,
+Address.state
+FROM Person
+LEFT JOIN Address
+ON Person.personId = Address.personId;
